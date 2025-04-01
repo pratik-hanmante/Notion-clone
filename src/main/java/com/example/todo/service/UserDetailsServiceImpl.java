c5bf76f07fb2be718f84a4f241ad7b2138ec206d
pratik-hanmante <84140896+pratik-hanmante@users.noreply.github.com>
2025-04-01 23:42:28 +0530

    Update UserDetailsServiceImpl.java

--- src/main/java/com/example/todo/service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/todo/service/UserDetailsServiceImpl.java
@@ -1,30 +1,43 @@
 package com.example.todo.service;
 
-import com.example.todo.model.User;
-import com.example.todo.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.core.userdetails.User.UserBuilder;
-import org.springframework.stereotype.Service;
+import com.example.todo.model.User; // Importing the User entity
+import com.example.todo.repository.UserRepository; // Importing the UserRepository interface
+import org.springframework.beans.factory.annotation.Autowired; // Importing the Autowired annotation
+import org.springframework.security.core.userdetails.UserDetails; // Importing the UserDetails interface
+import org.springframework.security.core.userdetails.UserDetailsService; // Importing the UserDetailsService interface
+import org.springframework.security.core.userdetails.UsernameNotFoundException; // Importing the UsernameNotFoundException class
+import org.springframework.security.core.userdetails.User.UserBuilder; // Importing the UserBuilder class
+import org.springframework.stereotype.Service; // Importing the Service annotation
 
-@Service
+@Service // Indicates that this class is a service component in the Spring context
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserRepository userRepository; // Injecting the UserRepository dependency
 
+    /**
+     * Loads the user by username.
+     *
+     * @param username the username of the user to be loaded
+     * @return UserDetails object containing user information
+     * @throws UsernameNotFoundException if the user is not found in the repository
+     */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // Retrieve the User object for the given username
         User user = userRepository.findByUsername(username);
+        
+        // If the user is not found, throw an exception
         if (user == null) {
             throw new UsernameNotFoundException("User not found.");
         }
+        
+        // Create a UserBuilder object to build the UserDetails
         UserBuilder builder = org.springframework.security.core.userdetails.User.withUsername(username);
-        builder.password(user.getPassword());
-        builder.roles(user.getRole());
+        builder.password(user.getPassword()); // Set the password for the UserDetails
+        builder.roles(user.getRole()); // Set the roles for the UserDetails
+        
+        // Return the built UserDetails object
         return builder.build();
     }
 }
-
